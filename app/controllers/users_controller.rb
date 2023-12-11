775f6e7908eeb112fc4264d9e238332cd7307516
Patryk Rogala <rogala.patryk@pm.me>
2023-12-11 22:20:38 +0100

    Add user routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit]
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+end
