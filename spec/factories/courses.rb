9e85f5fdcc1530558539866667aa7b2a08a0b24c
Patryk Rogala <rogala.patryk@pm.me>
2023-12-17 20:33:28 +0100

    Add image attachment to course factory

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :course do
     name { Faker::Educator.course_name }
     description { Faker::Lorem.paragraph }
+    after(:build) do |course|
+      course.image.attach(io: File.open(Rails.root.join('spec', 'factories', 'images', 'course.png')),
+                          filename: 'course.png', content_type: 'image/png')
+    end
   end
 end
