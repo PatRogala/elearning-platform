efee398567ba2c1468bf7f5af7a855cec886fbd8
Patryk Rogala <rogala.patryk@pm.me>
2023-12-17 15:46:10 +0100

    Refactor panel_helper_spec.rb tests

--- spec/helpers/admin/panel_helper_spec.rb
+++ spec/helpers/admin/panel_helper_spec.rb
@@ -2,18 +2,27 @@ require "rails_helper"
 
 RSpec.describe Admin::PanelHelper do
   describe "#panels" do
-    it "returns an array of hashes" do
+    it "returns an array" do
       expect(helper.panels).to be_an Array
+    end
+
+    it "returns an array of hashes" do
       expect(helper.panels.first).to be_a Hash
     end
 
-    it "returns a hash with a title and path" do
+    it "returns a hash with a title" do
       expect(helper.panels.first).to have_key :title
+    end
+
+    it "returns a hash with a path" do
       expect(helper.panels.first).to have_key :path
     end
 
-    it "returns a hash with a title and path that are strings" do
+    it "returns a hash with a title that is string" do
       expect(helper.panels.first[:title]).to be_a String
+    end
+
+    it "returns a hash with a path that is string" do
       expect(helper.panels.first[:path]).to be_a String
     end
   end
