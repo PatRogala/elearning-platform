3b5c797ef127e6e9cc38eb65f633536170af0abd
Patryk Rogala <rogala.patryk@pm.me>
2024-01-28 08:16:34 +0100

    Add delete users path, and user index table

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,3 +1,27 @@
 class Admin::UsersController < Admin::ApplicationController
-  def index; end
+  before_action :dont_delete_admin_user, only: [:destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+
+    if @user.destroy
+      redirect_to admin_users_path, notice: "User deleted."
+    else
+      redirect_to admin_users_path, alert: "User could not be deleted."
+    end
+  end
+
+  private
+
+  def dont_delete_admin_user
+    @user = User.find_by(id: params[:id])
+
+    if @user.admin?
+      redirect_to admin_users_path, alert: "Cannot delete admin user."
+    end
+  end
 end
