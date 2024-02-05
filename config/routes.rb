57893d59c5055c36e01307c04b333c787adec2ca
Patryk Rogala <rogala.patryk@pm.me>
2024-02-05 11:41:22 +0100

    Add custom routes for user profile, admin panel, and FAQ page

--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,31 @@ Rails.application.routes.draw do
     resources :courses, only: [:index, :new, :create]
   end
 
-  # Custom routes
-  get "/faq", to: "home#faq", as: :faq
+  ####
+  # Custom routes start here
+  #
+  # For each new custom route:
+  #
+  # * Be sure you have the canonical route declared above
+  # * Add the new custom route below the existing ones
+  # * Document why it's needed
+  # * Explain anything else non-standard
+
+  # Used to display singular profile page for the current user
   get "/profile", to: "users#show", as: :profile
+
+  # Used to display the edit profile page for the current user
   get "/profile/edit", to: "users#edit", as: :edit_profile
+
+  # Used to update the current user's profile
   put "/profile", to: "users#update"
+
+  # Used to display main admin panel
   get "/admin", to: "admin/panel#index", as: :admin
+
+  # Used to display the FAQ page
+  get "/faq", to: "home#faq", as: :faq
+
+  # Used to display the privacy policy page
   get "/cookies", to: "home#cookies", as: :cookies
 end
