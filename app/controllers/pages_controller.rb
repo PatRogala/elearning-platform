5f2026f902887588e7d7e2c81bfef053b61ab8a9
Patryk Rogala <rogala.patryk@pm.me>
2024-03-09 14:55:27 +0100

    Add slug validation to PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def show
-    render template: "pages/#{params[:slug]}"
+    valid_slugs = %w[home faq cookie-policy]
+    if valid_slugs.include?(params[:slug])
+      render template: "pages/#{params[:slug]}"
+    else
+      render file: "#{Rails.root}/public/404.html", status: :not_found
+    end
   end
 
   def faq
