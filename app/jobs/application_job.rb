a115493349891fcb767f4082a2165e1592dde54c
Patryk Rogala <rogala.patryk@pm.me>
2024-03-11 23:27:51 +0100

    Refactor ApplicationJob to use Sidekiq

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-class ApplicationJob < ActiveJob::Base
-  # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
+# Do not inherit from ActiveJob. All jobs use Sidekiq
+class ApplicationJob
+  include Sidekiq::Worker
 
-  # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
+  sidekiq_options backtrace: true
 end
